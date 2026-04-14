e89e2482248316203bdc562dbea09a35267dba90
nguyen34 <nguyen_quoc_huy@hotmail.ca>
2026-04-13 17:42:51 -0700

    Re-worked search to no longer rely on submit button

--- products/forms.py
+++ products/forms.py
@@ -3,6 +3,6 @@ from products.models import Tag, Category
 
 # Query form to filter by description, tag and category
 class ProductFilterForm(forms.Form):
-    query = forms.CharField(label="Search by Description", max_length=300, required=False)
-    tag = forms.ModelChoiceField(label="Tags", queryset=Tag.objects.all(), required=False)
-    category = forms.ModelChoiceField(label="Categories", queryset=Category.objects.all(), required=False)
+    query = forms.CharField(label="Search by Description", max_length=300, required=False, widget=forms.TextInput(attrs={'onchange': 'submit();'}))
+    tag = forms.ModelChoiceField(label="Tags", queryset=Tag.objects.all(), required=False, widget=forms.Select(attrs={'onchange': 'submit();'}))
+    category = forms.ModelChoiceField(label="Categories", queryset=Category.objects.all(), required=False, widget=forms.Select(attrs={'onchange': 'submit();'}))
